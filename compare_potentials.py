78c0a66951aded2eaefa7a820736dec4073de3c2
lukaselflein <elfleinl@cs.uni-freiburg.de>
2019-07-09 14:06:11 +0200

    convergence analysis

--- compare_potentials.py
+++ compare_potentials.py
@@ -165,31 +165,13 @@ def reject_sample(atom_positions, dft_esp, grid_vectors, upper_bound, lower_boun
 
    return probe_positions
    
-
-def main(verbose=False):
-   if verbose: print('Parsing cubefile ...')
-   atom_positions, dft_esp, grid_vectors = parse_cubefile(path='esp.cube')
-   upper_bound = 7
-   lower_bound = 1.8 
-
-   #n_samples = 1000000 # takes 7.4 hours
-   n_samples = 1000
-
-   # Look up DFT eletrostatic potential at probe positions
-
-   # Calculate HORTON potential
-   if verbose: print('Parsing HORTON charges ...')
-   charge_df = parse_charges()
-   if verbose: print('Getting ase - pmd conversion ...')
-   pmd2ase, ase2pmd = create_structure()
-
-
+def sweep_samplesize(atom_positions, dft_esp, grid_vectors, upper_bound, lower_bound,
+                     n_samples, charge_df, pmd2ase):
    start = time.time()
 
    # Get the non-rejected probe spots in units of cubefiles line-numbers 
    probe_positions = reject_sample(atom_positions, dft_esp, grid_vectors, 
                                    upper_bound, lower_bound, n_samples)
- 
    # Set up a table 
    df = pd.DataFrame() 
    df['dft_lines'] = probe_positions
@@ -203,23 +185,75 @@ def main(verbose=False):
       dft_esp_at_probe += [float(dft_esp[line].strip()) * -1]
    df['dft_esp'] = dft_esp_at_probe
 
-   if verbose: print('Calculating HORTON ESP')
    charge_xyz = combine_data(charge_df, atom_positions, pmd2ase)
    horton_esp_at_probe = []
    for line in probe_positions:
       horton_esp_at_probe += [get_esp(charge_xyz, line_to_xyz(dft_esp, line, grid_vectors))]
    df['horton_esp'] = horton_esp_at_probe
-   if verbose: print(df)
 
    df['square_dev'] = (df['horton_esp'] - df['dft_esp']).pow(2)
    rrmsd = np.sqrt(df.square_dev.mean())
 
-   with open('quality.csv', 'a') as outfile:
-      #outfile.write('{}, {}\n'.format(rrmsd, df['horton_esp'].corrwith(df['dft_esp'])))
-      print('{}, {}\n'.format(rrmsd, df['horton_esp'].corr(df['dft_esp'])))
+   with open('convergence.csv', 'a') as outfile:
+      correlation = df['horton_esp'].corr(df['dft_esp'])
+      run_time = time.time() - start
+      outfile.write('{}, {}, {}, {}\n'.format(n_samples, rrmsd, correlation, run_time))
+      #print('{}, {}, {}, {}\n'.format(n_samples, rrmsd, correlation, run_time))
   
-   if verbose: print('{} samples done in {}'.format(n_samples, time.time() - start))
-   if verbose: print('Done')
+
+def main(sweep=False):
+   if not sweep: print('Parsing cubefile ...')
+   atom_positions, dft_esp, grid_vectors = parse_cubefile(path='esp.cube')
+   upper_bound = 7
+   lower_bound = 1.8 
+
+   #n_samples = 1000000 # takes 7.4 hours
+   n_samples = 1000
+
+   # Look up DFT eletrostatic potential at probe positions
+
+   # Calculate HORTON potential
+   if not sweep : print('Parsing HORTON charges ...')
+   charge_df = parse_charges()
+   if not sweep: print('Getting ase - pmd conversion ...')
+   pmd2ase, ase2pmd = create_structure()
+
+   if sweep:
+      for n_samples in [100, 500, 1000, 2500, 5000, 7500, 10000, 10250]:
+         sweep_samplesize(atom_positions, dft_esp, grid_vectors, upper_bound, 
+                          lower_bound, n_samples, charge_df, pmd2ase)
+      return
+
+   print('Rejection sampling of esp locations, n = {} ...'.format(n_samples))
+   # Get the non-rejected probe spots in units of cubefiles line-numbers 
+   probe_positions = reject_sample(atom_positions, dft_esp, grid_vectors, 
+                                   upper_bound, lower_bound, n_samples)
+   # Set up a table 
+   df = pd.DataFrame() 
+   df['dft_lines'] = probe_positions
+   xyz = [line_to_xyz(dft_esp, n, grid_vectors) for n in probe_positions]
+   df['x'] = [pos[0] for pos in xyz]
+   df['y'] = [pos[1] for pos in xyz]
+   df['z'] = [pos[2] for pos in xyz]
+
+   dft_esp_at_probe = []
+   for line in probe_positions:
+      dft_esp_at_probe += [float(dft_esp[line].strip()) * -1]
+   df['dft_esp'] = dft_esp_at_probe
+
+   print('Calculating HORTON ESP ...')
+   charge_xyz = combine_data(charge_df, atom_positions, pmd2ase)
+   horton_esp_at_probe = []
+   for line in probe_positions:
+      horton_esp_at_probe += [get_esp(charge_xyz, line_to_xyz(dft_esp, line, grid_vectors))]
+   df['horton_esp'] = horton_esp_at_probe
+
+   print('Calculating statistics ...')
+   df['square_dev'] = (df['horton_esp'] - df['dft_esp']).pow(2)
+   rrmsd = np.sqrt(df.square_dev.mean())
+   correlation = df['horton_esp'].corr(df['dft_esp'])
+
+   print('n = {}, rrmsd = {}, corr = {}'.format(n_samples, rrmsd, correlation))
 
 if __name__ == '__main__':
    main()
