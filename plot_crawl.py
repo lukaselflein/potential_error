2c814e8022bcf5a3c74005e3226510c088dd8702
lukaselflein <elfleinl@cs.uni-freiburg.de>
2019-07-10 16:38:56 +0200

    plot

--- plot_crawl.py
+++ plot_crawl.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from smamp.tools import find
 
-# Load data (wide format)
+print('Load data (wide format) ...')
 data_paths = find(path='.', folder_keyword='/data', file_keyword='csv', nr_occ=None)
 
 collect_df = pd.DataFrame()
@@ -18,12 +18,15 @@ for data_file in data_paths:
    df = pd.read_csv(data_file)
    collect_df = collect_df.append(df)
 
+print('Manipulating data ...')
 df = collect_df.reset_index(drop=True)
 df.columns = df.columns.map(str.strip)
 df['lnrho'] = pd.to_numeric(df['lnrho'])
 df['sigma'] = df['sigma'].apply(lambda x: str(x))
 df = df.loc[df.lnrho < -3]
 
+print('Plotting ...')
 p = sns.pointplot(data=df, x='lnrho', y='rrmsd', hue='sigma', palette='GnBu_d')
 #p = sns.pointplot(data=df, x='sigma', y='rrmsd', hue='lnrho', palette='GnBu_d')
-plt.show()
+p.figure.savefig('lnrho_vs_rrmsd.png')
+print('Done.')
